e514d2f9933b6bf3e179d646d60e07116777534e
mai19950 <underdog-95@outlook.com>
2024-12-01 13:16:01 +0800

    feat: change local transform

--- main.py
+++ main.py
@@ -1,31 +1,139 @@
+import base64
+import os
+import re
 import requests
+import yaml
+from typing import Any, List, Dict, Optional, Union
 
+from parse_node import CollectNodes
 
-# url = "https://suo.yt/0PKQ4Yf"
-url = ("https://sub.xeton.dev/sub?target=clash&new_name=true&url="
-       "https://github.com/mai19950/free_site/raw/refs/heads/main/v2ray/base64|"
-       "https://raw.githubusercontent.com/ripaojiedian/freenode/main/sub|"
-       "https://vless.fxxk.dedyn.io/auto"
-       "&insert=false"
-       "&config=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_AdblockPlus.ini")
 
 headers = {
   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
 }
 
-def main():
-  try:
-    with requests.get(url, timeout=10, headers=headers) as res:
-      if res.status_code < 300:
-        with open('clash.yaml', mode="w+", encoding="utf-8") as f:
-          f.write(res.text)
-          print("节点保存成功")
-      else:
-        print("链接请求错误：", res.status_code)
-  except Exception as e:
-    print(e.args)
-    main()
+class ClashConfig:
+  
+  @staticmethod
+  def read_yaml(file: str) -> Optional[Dict]:
+    with open(file, 'r', encoding="utf-8") as f:
+      return yaml.safe_load(f)
+  
+  @staticmethod
+  def write_yaml(data: Dict, file: str) -> None:
+    with open(file, 'w+', encoding="utf-8") as f:
+      yaml.dump(data, f, default_flow_style=False, allow_unicode=True)
+
+  @staticmethod
+  def read_local(file: str) -> str:
+    with open(file, "r", encoding="utf-8") as f:
+      nodes = f.read().strip()
+      try:
+        d_bytes = base64.b64decode(nodes)
+        return d_bytes.decode('utf-8')
+      except Exception as e:
+        print('解码错误：', e.args)
+        return nodes
+
+  configs: dict = {}
+  local_dir = 'local_nodes'
+  o_node = CollectNodes()
+  with open("./urls.txt", "r", encoding="utf-8") as urls_path:
+    urls = urls_path.readlines()
+
+  @classmethod
+  def to_yaml(cls, data: Any, indent: int = 0) -> str:
+    """
+    递归地将数据转换为 YAML 格式的字符串。
+    
+    :param data: 数据，可能是字典、列表或其他类型。
+    :param indent: 当前的缩进级别，用于格式化输出。
+    :return: 生成的 YAML 格式字符串。
+    """
+    indent_str = ' ' * indent
+    if isinstance(data, dict):
+      yaml_str = '\n'
+      for key, value in data.items():
+        yaml_str += f"{indent_str}{key}: {cls.to_yaml(value, indent + 2)}\n"
+      return yaml_str
+    elif isinstance(data, list):
+      yaml_str = '\n'
+      for item in data:
+        yaml_str += f"{indent_str}- {cls.to_yaml(item, indent + 2)}\n"
+      return yaml_str
+    elif data is None:
+      return "null"
+    else:
+      return str(data)
+
+  @classmethod
+  def get_scribe(cls, url: str) -> str:
+    try:
+      with requests.get(url, timeout=10, headers=headers) as res:
+        if res.status_code < 300:
+          print("节点请求成功: ", url)
+          nodes = res.text.strip()
+          try:
+            d_bytes = base64.b64decode(nodes)
+            return d_bytes.decode('utf-8')
+          except Exception as e:
+            print('解码错误：', e.args)
+            return nodes
+        else:
+          print("链接请求错误：", res.status_code, url)
+          return None
+    except Exception as e:
+      print(e.args)
+      return cls.get_scribe(url)
+
+  @classmethod
+  def run(cls):
+    cls.configs = cls.read_yaml('./yaml_files/configs.yaml')
+    # rules = cls.read_yaml('./yaml_files/rules.yaml')
+    proxy_groups = cls.read_yaml('./yaml_files/proxy_groups.yaml')["proxy-groups"]
+
+    for url in cls.urls:
+      cc = cls.get_scribe(url.strip())
+      if cc:
+        cls.o_node.parse(cc.split('\n'))
+    
+    for file in os.listdir(cls.local_dir):
+      cc = cls.read_local(os.path.join(cls.local_dir,  file))
+      if cc:
+        cls.o_node.parse(cc.split('\n'))
+
+    if cls.o_node.nodes:
+      cls.configs["proxies"] = cls.o_node.nodes
+      proxy_groups[1]["proxies"] = cls.o_node.remarks
+      proxy_groups[2]["proxies"] = cls.o_node.remarks
+      proxy_groups[3]["proxies"] = cls.o_node.remarks
+      proxy_groups[4]["proxies"] = cls.o_node.remarks
+      # cls.configs["rules"] = cls.read_yaml('./yaml_files/rules.yaml')["rules"]
+    if cls.o_node.HK_remarks:
+      proxy_groups[26]["proxies"] = cls.o_node.HK_remarks
+    if cls.o_node.JP_remarks:
+      proxy_groups[27]["proxies"] = cls.o_node.JP_remarks
+    if cls.o_node.US_remarks:
+      proxy_groups[28]["proxies"] = cls.o_node.US_remarks
+    if cls.o_node.TW_remarks:
+      proxy_groups[29]["proxies"] = cls.o_node.TW_remarks
+    if cls.o_node.SG_remarks:
+      proxy_groups[30]["proxies"] = cls.o_node.SG_remarks
+    if cls.o_node.KR_remarks:
+      proxy_groups[31]["proxies"] = cls.o_node.KR_remarks
+    cls.configs["proxy-groups"] = proxy_groups
+
+
+    # cls.write_yaml(cls.configs, 'clash.yaml')
+    yaml_str = re.sub(r'[\r\n]+', '\n', cls.to_yaml(cls.configs), flags=re.S) 
+    yaml_str = re.sub(r'^(.*?-)[\s\r\n]+', r'\1 ', yaml_str, flags=re.M)
+    with open('clash.yaml', "w+", encoding="utf-8") as f:
+      f.write(yaml_str)
+    os.system("cat yaml_files/rules.yaml >> clash.yaml")
+
+
 
 
 if __name__ == '__main__':
-  main()
+  # main()
+  ClashConfig.run()
---
+++ parse_node.py
@@ -0,0 +1,245 @@
+import base64
+import json
+import re
+import urllib.parse
+from typing import Dict, Tuple
+
+
+class ParseNode:
+
+  @staticmethod
+  def ss(url: str) -> Tuple[str, dict]:
+    ss_link = url[5:]  # 去掉 "ss://"
+    # 进行 Base64 解码，添加必要的填充
+    base64_str = ss_link.split('@')[0]
+    padding = len(base64_str) % 4
+    if padding != 0:
+      base64_str += '=' * (4 - padding)
+    # 进行 Base64 解码
+    decoded_bytes = base64.b64decode(base64_str)
+    decoded_string = decoded_bytes.decode('utf-8')
+
+    # 提取加密方式和密码
+    server, port_with_remark = ss_link.split('@')[1].split(':', 1)
+    method, password = decoded_string.split(':')
+    port, remark = port_with_remark.split('#')
+    remarks = urllib.parse.unquote(remark).strip()
+
+    # 构建 Clash 配置
+    clash_node = {
+      "name": remarks,
+      "server": server.rsplit(":", 1)[0],
+      "port": port,
+      "type": "ss",
+      "cipher": method,
+      "password": password
+    }
+
+    return ( remarks, clash_node )
+
+  @staticmethod
+  def vless(url: str) -> Tuple[str, dict]:
+    vless_link = url[8:]  # 去掉 "vless://"
+    # 分离 UUID 和其他信息
+    uuid_info, rest_remark = vless_link.split('@')
+    rest, remarks = rest_remark.split('#')
+    uuid = uuid_info
+    # 解析主机地址和端口
+    server_info, query = rest.split('?')
+    server, port = server_info.rsplit(":", 1)
+    port = int(port)
+    # 解析查询参数
+    params = urllib.parse.parse_qs(query)    
+    # 获取查询参数（加密方法，安全性，sni 等）
+    encryption = params.get('encryption', ['none'])[0]
+    security = params.get('security', ['tls'])[0]
+    sni = params.get('sni', [''])[0]
+    alpn = params.get('alpn', [''])[0]
+    fp = params.get('fp', [''])[0]
+    host = params.get('host', [''])[0]
+    path = urllib.parse.unquote(params.get('path', [''])[0])
+    # 提取备注
+    remarks = urllib.parse.unquote(remarks).strip()
+    # 构建 Clash 配置
+    clash_node = {
+      "type": "vless",
+      "name": remarks,
+      "server": server,
+      "port": port,
+      "uuid": uuid,
+      "skip-cert-verify": True,  # 跳过证书验证
+      "udp": True,  # 支持 UDP
+      "tls": True if security == "tls" else False,  # TLS 安全连接
+      "network": "ws",  # WebSocket 协议
+      "servername": sni,  # 服务器名称
+      "ws-opts": {
+        "path": path,  # WebSocket 路径
+        "headers": {
+          "host": host  # WebSocket 请求头的 host
+        }
+      }
+    }
+    return ( remarks, clash_node )
+
+  @staticmethod
+  def trojan(url: str) -> Tuple[str, dict]:
+    trojan_link = url[8:]  # 去掉 "trojan://"
+    # 分离密码和其他信息
+    password_info, rest = trojan_link.split('@')
+    password = password_info
+    # 解析主机地址和端口
+    server_info, query = rest.split('?')
+    server, port = server_info.rsplit(":", 1)
+    port = int(port.strip('/'))
+    
+    # 解析查询参数
+    params = urllib.parse.parse_qs(query)
+    
+    # 获取查询参数（如安全性，类型，头部等）
+    security = params.get('security', ['tls'])[0]
+    header_type = params.get('headerType', ['none'])[0]
+    network_type = params.get('type', ['tcp'])[0]
+    
+    # 提取备注
+    remarks = trojan_link.split('#')[-1]  # 备注在 # 后面
+    remarks = urllib.parse.unquote(remarks)  # URL 解码
+    # 构建 Clash 配置
+    clash_node = {
+      "name": remarks,
+      "server": server,
+      "port": port,
+      "type": "trojan",
+      "password": password,
+      "skip-cert-verify": True,  # 跳过证书验证
+      "security": security,  # 使用传入的 security 参数
+      "headerType": header_type,  # 使用传入的 headerType 参数
+      "network": network_type,  # 使用传入的协议类型
+    }
+    return ( remarks, clash_node )
+
+  @staticmethod
+  def vmess(url: str) -> Tuple[str, dict]:
+    vmess_link = url[8:]  # 去掉 "vmess://"
+    
+    # Base64 解码
+    decoded = base64.b64decode(vmess_link).decode('utf-8')
+    
+    # 将解码后的内容转为字典
+    vmess_data = json.loads(decoded)
+    
+    # 提取需要的字段
+    remarks = vmess_data.get("ps", "")  # 从 vmess 数据中提取 ps（备注）
+    server = vmess_data.get("add", "")
+    port = vmess_data.get("port", 0)
+    uuid = vmess_data.get("id", "")
+    alter_id = vmess_data.get("aid", 0)
+    cipher = vmess_data.get("scy", "auto")
+    tls = vmess_data.get("tls", "false") == "true"  # 将 tls 设置为布尔值
+    remarks = urllib.parse.unquote(remarks).strip()
+    
+    # 构建 Clash 配置
+    clash_node = {
+      "name": remarks,
+      "server": server,
+      "port": port,
+      "type": "vmess",
+      "uuid": uuid,
+      "alterId": alter_id,
+      "cipher": cipher,
+      "tls": tls
+    }
+    return ( remarks, clash_node )
+
+
+class CollectNodes(ParseNode):
+  nodes = []  
+  HK_nodes = [] # 香港节点
+  JP_nodes = [] # 日本节点
+  US_nodes = [] # 美国节点
+  TW_nodes = [] # 台湾节点 
+  SG_nodes = [] # 狮城节点
+  KR_nodes = [] # 韩国节点
+
+  remarks = []  
+  HK_remarks = [] # 香港节点
+  JP_remarks = [] # 日本节点
+  US_remarks = [] # 美国节点
+  TW_remarks = [] # 台湾节点 
+  SG_remarks = [] # 狮城节点
+  KR_remarks = [] # 韩国节点
+
+  node_list = [ "nodes", "HK_nodes", "JP_nodes", "US_nodes", "TW_nodes", "SG_nodes", "KR_nodes" ]
+  remark_list = [ "remarks", "HK_remarks", "JP_remarks", "US_remarks", "TW_remarks", "SG_remarks", "KR_remarks" ]
+
+  keys_map = {}
+
+  parse_node = lambda x: x
+
+  def parse(self, urls: list):
+    for url in urls:
+      url = url.strip()
+      if url == "":
+        continue
+      elif "edtunnel" in url:
+        continue
+      elif url.startswith("ss://"):
+        self.parse_node = self.ss
+      elif url.startswith("vless://"):
+        self.parse_node = self.vless
+      elif url.startswith("trojan://"):
+        self.parse_node = self.trojan
+      elif url.startswith("vmess://"):
+        self.parse_node = self.vmess
+      else:
+        continue
+      try:
+        remark, node = self.parse_node(url)
+      except Exception as e:
+        print("节点转换失败：", e.args)
+        print(url)
+        continue
+      if remark in self.keys_map:
+        node["name"] = f"{remark}_{self.keys_map[remark]}"
+        self.keys_map[remark] += 1
+      else:
+        self.keys_map[remark] = 1
+
+      node_str = json.dumps(node, ensure_ascii=False)
+      self.nodes.append(node_str)
+      remark_with = node["name"]
+      if " " in node["name"]:
+        remark_with = f'"{remark_with}"'
+
+      self.remarks.append(remark_with)
+      
+      if re.search(r'HK|香|港|香港|🇭🇰', remark_with, flags=re.I):
+        self.HK_nodes.append(node_str)
+        self.HK_remarks.append(remark_with)
+      elif re.search(r'JP|日|日本|🇯🇵', remark_with, flags=re.I):
+        self.JP_nodes.append(node_str)
+        self.JP_remarks.append(remark_with)
+      elif re.search(r'US|UM|美|美国|美國|🇺🇲', remark_with, flags=re.I):
+        self.US_nodes.append(node_str)
+        self.US_remarks.append(remark_with)
+      elif re.search(r'TW|台|臺|台湾|臺灣|🇨🇳|🇹🇼', remark_with, flags=re.I):
+        self.TW_nodes.append(node_str)
+        self.TW_remarks.append(remark_with)
+      elif re.search(r'SG|新|狮城|獅城|新加坡|🇸🇬', remark_with, flags=re.I):
+        self.SG_nodes.append(node_str)
+        self.SG_remarks.append(remark_with)
+      elif re.search(r'KR|韩|韩国|韓國|🇰🇷', remark_with, flags=re.I):
+        self.KR_nodes.append(node_str)
+        self.KR_remarks.append(remark_with)
+        
+    print(f"节点总数: {len(self.nodes)}\t"
+          f"HK: {len(self.HK_nodes)}\t"
+          f"JP: {len(self.JP_nodes)}\t"
+          f"US: {len(self.US_nodes)}\t"
+          f"TW: {len(self.TW_nodes)}\t"
+          f"SG: {len(self.SG_nodes)}\t"
+          f"KR: {len(self.KR_nodes)}")
+    return self
+
+
+
+
